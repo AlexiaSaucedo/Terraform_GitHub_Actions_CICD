c0fb138f6f286c3d367c68c9d6be14a34c4f67c6
Alexia Saucedo Romero <72261496+AlexiaSaucedo@users.noreply.github.com>
2026-03-03 10:11:41 -0600

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "alexia-s3bucket-03march"
+    bucket         = "alexia-s3bucket-3march"
     key            = "globalstate/s3/terraform.tfstate"
     region         = "eu-west-1"
     use_lockfile   = "alexia-03march-dblocks"
